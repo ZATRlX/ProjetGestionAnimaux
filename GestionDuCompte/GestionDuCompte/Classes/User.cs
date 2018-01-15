e56de5efe73057bb15a9dd74d2dc803ac6ea419a
Imacutekayx <imkayxy@gmail.com>
2018-01-15 11:05:17 +0100

    Création des requêtes SQL
    Création des requêtes SQL et implémentation dans les méthodes + Code Test de connection à la bd

--- GestionDuCompte/GestionDuCompte/Classes/User.cs
+++ GestionDuCompte/GestionDuCompte/Classes/User.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
 
 namespace GestionDuCompte.Classes
 {
@@ -17,6 +20,7 @@ namespace GestionDuCompte.Classes
         private string mail;
         private string password;
         private string passPhrase;
+        private string answer;
         //TODO Faire une liste de la classe animal
         private List<string> lstAnimals = new List<string>();
         private List<Dictionary<DateTime, string>> lstPublic = new List<Dictionary<DateTime, string>>();
@@ -82,6 +86,17 @@ namespace GestionDuCompte.Classes
                 passPhrase = value;
             }
         }
+        public string Answer
+        {
+            get
+            {
+                return answer;
+            }
+            set
+            {
+                answer = value;
+            }
+        }
         public List<string> LstAnimals
         {
             get
@@ -139,13 +154,14 @@ namespace GestionDuCompte.Classes
         }
 
         //Constructeur
-        public User(string name, string pseudo, string mail,string password, string passPhrase, List<string> lstAnimals, List<Dictionary<DateTime, string>> lstPublic, string strPreferenceColor, int profilePicture, int agendaUser)
+        public User(string name, string pseudo, string mail,string password, string passPhrase, string answer, List<string> lstAnimals, List<Dictionary<DateTime, string>> lstPublic, string strPreferenceColor, int profilePicture, int agendaUser)
         {
             Name = name;
             Pseudo = pseudo;
             Mail = mail;
             Password = password;
             PassPhrase = passPhrase;
+            Answer = answer;
             LstAnimals = lstAnimals;
             LstPublic = lstPublic;
             StrPreferenceColor = strPreferenceColor;
@@ -154,15 +170,27 @@ namespace GestionDuCompte.Classes
             lstUser.Add(this);
         }
 
-        //TODO Utiliser le pseudo comme repère dans la base de donnée
-        //TODO METHODES A METTRE DANS LE MODE & LIER A LA BD
+        //TODO Pseudo utilisé comme repère
+        //TODO METHODES A METTRE DANS LE MODEL & LIER A LA BD
         public void ActualizeUser()
         {
-
+            //TODO Ajouter les variables restantes à la BD
+            currentUser.Name = Convert.ToString(ExecuteQuery(@"SELECT Name FROM t_user WHERE Pseudo = " + currentUser.Pseudo + ";"));
+            currentUser.Mail = Convert.ToString(ExecuteQuery(@"SELECT Mail FROM t_user WHERE Pseudo = " + currentUser.Pseudo + ";"));
+            currentUser.Password = Convert.ToString(ExecuteQuery(@"SELECT Password FROM t_user WHERE Pseudo = " + currentUser.Pseudo + ";"));
+            currentUser.PassPhrase = Convert.ToString(ExecuteQuery(@"SELECT PassPhrase FROM t_user WHERE Pseudo = " + currentUser.Pseudo + ";"));
+            currentUser.Answer = Convert.ToString(ExecuteQuery(@"SELECT Answer FROM t_user WHERE Pseudo = " + currentUser.Pseudo + ";"));
+            currentUser.ProfilePicture = Convert.ToInt32(ExecuteQuery(@"SELECT ProfilePicture FROM t_user WHERE Pseudo = " + currentUser.Pseudo + ";"));
         }
         public void SaveUserChanges()
         {
-
+            //TODO Ajouter les variables restantes à la BD
+            ExecuteQuery(@"UPDATE t_user SET Name = " + currentUser.Name + "WHERE Pseudo = " + currentUser.Pseudo + ";");
+            ExecuteQuery(@"UPDATE t_user SET Mail = " + currentUser.Mail + "WHERE Pseudo = " + currentUser.Pseudo + ";");
+            ExecuteQuery(@"UPDATE t_user SET Password = " + currentUser.Password + "WHERE Pseudo = " + currentUser.Pseudo + ";");
+            ExecuteQuery(@"UPDATE t_user SET PassPhrase = " + currentUser.PassPhrase + "WHERE Pseudo = " + currentUser.Pseudo + ";");
+            ExecuteQuery(@"UPDATE t_user SET Answer = " + currentUser.Answer + "WHERE Pseudo = " + currentUser.Pseudo + ";");
+            ExecuteQuery(@"UPDATE t_user SET ProfilePicture = " + currentUser.ProfilePicture + "WHERE Pseudo = " + currentUser.Pseudo + ";");
         }
         public string GetUserName()
         {
@@ -204,6 +232,14 @@ namespace GestionDuCompte.Classes
         {
             currentUser.PassPhrase = passPhrase;
         }
+        public string GetUserAnswer()
+        {
+            return currentUser.Answer;
+        }
+        public void ChangeUserAnswer(string answer)
+        {
+            currentUser.Answer = answer;
+        }
         public List<string> GetLstAnimals()
         {
             return currentUser.LstAnimals;
@@ -222,5 +258,42 @@ namespace GestionDuCompte.Classes
             currentUser.LstAnimals.Remove(animal);
         }
         //TODO lstPublic, preferenceColor, profilPicture, agendaUser
+        
+
+        //HACK SQL
+        //string servant de connection à la base de donnée. Utilisé dans l'objet MySqlConnection.
+        static string connectionString = @"server=localhost;userid=root;password=;database=db_smartphones";
+
+        //Objet qui sera utilisé dans les méthodes qui récupèrent des données
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        //Objet "lecteur" de données 
+        SqlDataReader rdr;
+
+        /// <summary>
+        /// Execute les commandes SQL dans la base de donnée
+        /// </summary>
+        /// <param name="query">commande à executer</param>
+        /// <returns>MySqlDataReader rdr</returns>
+        private SqlDataReader ExecuteQuery(string query)
+        {
+            SqlCommand acmd = new SqlCommand(query, connection);
+
+            //Se connecte à la base de donnée
+            if (connection != null && connection.State == connectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            if (rdr != null)
+            {
+                rdr.Close();
+            }
+
+            //Execute la commande
+            rdr = acmd.ExecuteReader();
+
+            return rdr;
+        }
     }
 }
